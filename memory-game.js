e65b450c17a6fe0747b4e4693aab7658984a4e72
Hossein <Hossein.saadatpour@gmail.com>
2024-12-14 10:25:42 +0100

    add-3files

--- memory-game.js
+++ memory-game.js
@@ -2,8 +2,8 @@
 const cards=document.querySelectorAll(`.card`)
 for(let i=0; i<cards.length;i++){
 
-  const cardInner=cards[i].querySelector('.card-front-back')
-  cardInner.addEventListener(`click`,flip)
+  const cardInner=cards[i].querySelector('.card-front-back');
+  cardInner.addEventListener(`click`,flip);
 }
  function flip(eventFlip){
   const cardInner=eventFlip.currentTarget;
