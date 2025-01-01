42e4537c8fbb6cb298acff0b43834c03ea5d5084
Hossein <Hossein.saadatpour@gmail.com>
2025-01-01 13:24:17 +0100

    game is completed

--- cards.js
+++ cards.js
@@ -2,38 +2,47 @@ const cardsPic=[
   
   {
     id:1,
-    image:"/jscript2&3-project-pic/1.jpg"
+    image:"/jscript2&3-project-pic/1.jpg",
+    
   },
   {
     id:2,
-    image:"/jscript2&3-project-pic/2.jpg"
+    image:"/jscript2&3-project-pic/2.jpg",
+   
   },
   {
     id:3,
-    image:"/jscript2&3-project-pic/3.jpg"
+    image:"/jscript2&3-project-pic/3.jpg",
+    
   },
   {
     id:4,
-    image:"/jscript2&3-project-pic/4.jpg"
+    image:"/jscript2&3-project-pic/4.jpg",
+    
   },
   {
     id:5,
-    image:"/jscript2&3-project-pic/5.jpg"
+    image:"/jscript2&3-project-pic/5.jpg",
+   
   },
   {
     id:6,
-    image:"/jscript2&3-project-pic/6.jpg"
+    image:"/jscript2&3-project-pic/6.jpg",
+    
   },
   {
     id:7,
-    image:"/jscript2&3-project-pic/7.jpg"
+    image:"/jscript2&3-project-pic/7.jpg",
+   
   },
   {
     id:8,
-    image:"/jscript2&3-project-pic/8.jpg"
+    image:"/jscript2&3-project-pic/8.jpg",
+    
   },
   {
     id:9,
-    image:"/jscript2&3-project-pic/9.jpg"
+    image:"/jscript2&3-project-pic/9.jpg",
+    
   }
 ]
